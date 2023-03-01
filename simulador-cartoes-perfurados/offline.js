c5576cb961cd734aec49e3b202143889ce345e2e
Erlon Jr <ejrgeek@gmail.com>
2023-03-01 17:20:48 -0300

    feature: Aplicação criada para aula de organização e arquitetura de computadores

---
+++ simulador-cartoes-perfurados/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1677701057,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"images/tiledbackground7.png",
+		"images/tiledbackground8.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
